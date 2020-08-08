10eac05c0e4854dd15f4a63fa306e138c794e5c7
roca <rocaznet@gmail.com>
2020-08-09 01:11:09 +0900

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # probeCOCOATek
 Probe TemporaryExposureKeys and Files of Exposure Notifications System in Japan a.k.a. "COCOA".
+
+[![Python: 3.7+](https://img.shields.io/badge/Python-3.7+-4584b6.svg?style=popout&logo=python)](https://www.python.org/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Requirement
+
+- Python 3.7+
+
+## Usage
+
+probeCOCOATek.py [-h] [-v] [TEK]
+
+TEK:            TEK Zip URL. if not set, print TEK distrubuted list.
+
+-h, --help:     show this help message and exit
+
+-v, --version:  show program's version number and exit
+
+## Exsamples
+
+1. TEK Distributed List
+```$ python probeCOCOATek/probeCOCOATek.py```
+
+```
+#     Created                      TEK URL
+   0  [2020-07-25 11:00:09+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/366.zip]
+   1  [2020-07-25 16:00:18+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/389.zip]
+   2  [2020-07-25 17:00:15+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/402.zip]
+   3  [2020-07-25 18:00:11+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/415.zip]
+   4  [2020-07-25 21:00:08+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/416.zip]
+   :
+ 146  [2020-08-09 00:00:06+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/693.zip]
+ 147  [2020-08-09 00:00:06+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/694.zip]
+ 148  [2020-08-09 00:00:06+0900]   [https://covid19radar-jpn-prod.azureedge.net/c19r/440/695.zip]
+Total:       149
+```
+
+2. TEK Zip Detail
+```$ python probeCOCOATek/probeCOCOATek.py https://covid19radar-jpn-prod.azureedge.net/c19r/440/638.zip```
+
+```
+start_timestamp: [2020-07-27 09:00:00+0900]
+end_timestamp:   [2020-07-28 09:00:00+0900]
+region:          [440]
+batch_num:       [1]
+batch_size:      [1]
+signature_infos:
+    verification_key_version:      [v1]
+    verification_key_id:           [440]
+    signature_algorithm:           [1.2.840.10045.4.3.2]
+keys:  (Total:      [17])
+    [001]:[12e603645fd3475c4c74ee8bdebcb5af]
+       [transmission_risk_level       ]:[0]
+       [rolling_start_interval_number ]:[2659680]
+       [rolling_period                ]:[144]
+    [002]:[8a0fe13019472a31f3426a1c94c3eb1b]
+       [transmission_risk_level       ]:[0]
+       [rolling_start_interval_number ]:[2659680]
+       [rolling_period                ]:[144]
+    :
+```
+
+## License
+
+MIT
---
+++ probeCOCOATek/probeCOCOATek.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import io
+from urllib.parse import urlparse
+from datetime import datetime, timedelta, date
+from pytz import timezone
+from tzlocal import get_localzone
+import argparse
+import json
+import requests
+import zipfile
+import pandas as pd
+from google.protobuf.json_format import MessageToJson
+
+from TemporaryExposureKey.TemporaryExposureKey_pb2 import TemporaryExposureKeyExport
+
+import probeCOCOATek
+
+__version__ = '1.20200808'
+
+_tek_url = "https://covid19radar-jpn-prod.azureedge.net/c19r/440/list.json"
+_tek_key = "export.bin"
+_tek_sig = "export.sig"
+
+
+class Error(Exception):
+  pass
+class GitHubTokenError(Error):
+  def __init__(self, message):
+        self.message = message
+class AugumentError(Error):
+  def __init__(self, message):
+        self.message = message
+class ValueCleansingError(Error):
+  def __init__(self, message):
+        self.message = message
+class NoneValueError(Error):
+  def __init__(self, message):
+        self.message = message
+
+def _isURL(url) -> bool:
+    try:
+        u = urlparse(url)
+    except:
+        return False
+    return True
+
+def get_tek_json(tek_url) -> dict:
+    return requests.get(tek_url).json()
+
+def extract_key_zip(_zip_url):
+    r = requests.get(_zip_url, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    return z.read(_tek_key).strip("{:16}".format("EK Export v1").encode('utf-8')), z.read(_tek_sig)
+
+
+def normalize_dataframe(js) -> pd.DataFrame:
+    tek_list = []
+    for item in js:
+        tek = {"created":datetime.fromtimestamp(round(item["created"] / 1000)).astimezone(), "url": item["url"]}
+        tek_list.append(tek)
+    df = pd.DataFrame(tek_list)
+    df.index.name = "no"
+    df.reset_index(inplace=True)
+    df.set_index("created", inplace=True)
+    df.index = pd.to_datetime(df.index)
+    df.reset_index(inplace=True)
+    return df
+
+
+def print_tek_bin_full(tek_bin):
+    print("start_timestamp: [{:%Y-%m-%d %H:%M:%S%z}]".format(datetime.fromtimestamp(tek_bin.start_timestamp).astimezone()))
+    print("end_timestamp:   [{:%Y-%m-%d %H:%M:%S%z}]".format(datetime.fromtimestamp(tek_bin.end_timestamp).astimezone()))
+    print("region:          [{:}]".format(tek_bin.region))
+    print("batch_num:       [{:}]".format(tek_bin.batch_num))
+    print("batch_size:      [{:}]".format(tek_bin.batch_size))
+    print("signature_infos:")
+    print("    verification_key_version:      [{:}]".format(tek_bin.signature_infos[0].verification_key_version))
+    print("    verification_key_id:           [{:}]".format(tek_bin.signature_infos[0].verification_key_id))
+    print("    signature_algorithm:           [{:}]".format(tek_bin.signature_infos[0].signature_algorithm))
+    print("keys:  (Total:      [{:}])".format(len(tek_bin.keys)))
+    for i, k in enumerate(tek_bin.keys):
+        print("    [{:03d}]:[{:}]".format(i+1, k.key_data.hex()))
+        print("       [{:30}]:[{:}]".format("transmission_risk_level", str(k.transmission_risk_level)))
+        print("       [{:30}]:[{:}]".format("rolling_start_interval_number", str(k.rolling_start_interval_number)))
+        print("       [{:30}]:[{:}]".format("rolling_period", str(k.rolling_period)))
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Probe TemporaryExposureKeys and Files of Exposure Notifications System in Japan a.k.a. "COCOA".', prefix_chars='-/')
+    parser.add_argument('TEK', default=None, nargs='?', help="TEK Zip URL. If not set, print TEK distributed list")
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+    args = parser.parse_args(sys.argv[1:])
+
+    try:
+        if args.TEK is None:
+            df = normalize_dataframe(get_tek_json(_tek_url))
+            df_dict = df.to_dict(orient="records")
+
+            print("{0:4}  {1:28} {2:}".format("#", "Created", "TEK URL"))
+            for item in df_dict:
+                print("{0:4}  [{1:%Y-%m-%d %H:%M:%S%z}]   [{2:}]".format(item["no"], item["created"], item["url"]))
+            print("Total:{:>10}".format(len(df_dict)))
+
+        elif _isURL(args.TEK):
+            key_bin, _ = extract_key_zip(sys.argv[1])
+            tek_bin = TemporaryExposureKeyExport()
+            tek_bin.ParseFromString(key_bin)
+            print_tek_bin_full(tek_bin)
+        else:
+            print("Argument should be valid URL for TEK.")
+            sys.exit(1)
+
+    
+    except Exception as e:
+            print("Error happens, when getting TEK JSON.")
+            tb = sys.exc_info()[2]
+            print(e.with_traceback(tb))
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
