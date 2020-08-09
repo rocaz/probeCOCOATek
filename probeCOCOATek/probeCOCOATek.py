b7611c293ac0d4bee1427024d359c75bf1bf0331
rocaz <rocaznet@gmail.com>
2020-08-09 09:16:20 +0900

    add each-keys-count option

--- probeCOCOATek/probeCOCOATek.py
+++ probeCOCOATek/probeCOCOATek.py
@@ -19,7 +19,7 @@ import probeCOCOATek
 
 __version__ = '1.20200808'
 
-_tek_url = "https://covid19radar-jpn-prod.azureedge.net/c19r/440/list.json"
+_tek_distributed_url = "https://covid19radar-jpn-prod.azureedge.net/c19r/440/list.json"
 _tek_key = "export.bin"
 _tek_sig = "export.sig"
 
@@ -46,8 +46,8 @@ def _isURL(url) -> bool:
         return False
     return True
 
-def get_tek_json(tek_url) -> dict:
-    return requests.get(tek_url).json()
+def get_url_json(url) -> dict:
+    return requests.get(url).json()
 
 def extract_key_zip(_zip_url):
     r = requests.get(_zip_url, stream=True)
@@ -62,14 +62,30 @@ def normalize_dataframe(js) -> pd.DataFrame:
         tek_list.append(tek)
     df = pd.DataFrame(tek_list)
     df.index.name = "no"
+    df.index = df.index + 1
     df.reset_index(inplace=True)
     df.set_index("created", inplace=True)
     df.index = pd.to_datetime(df.index)
     df.reset_index(inplace=True)
     return df
 
+def print_tek_zip_list(tek_zip_list, ekc) -> None:
+    if ekc:
+        print("{0:4}  {1:28} {2:64} {3:}".format("#", "Created", "TEK URL", "Keys Count"))
+    else:
+        print("{0:4}  {1:28} {2:64}".format("#", "Created", "TEK URL"))
+    for item in tek_zip_list:
+        if ekc:
+            key_bin, _ = extract_key_zip(item["url"])
+            tek_bin = TemporaryExposureKeyExport()
+            tek_bin.ParseFromString(key_bin)
+            cnt = len(tek_bin.keys)
+            print("{0:4}  [{1:%Y-%m-%d %H:%M:%S%z}]   [{2:64}] [{3:4d}]".format(item["no"], item["created"], item["url"], cnt))
+        else:
+            print("{0:4}  [{1:%Y-%m-%d %H:%M:%S%z}]   [{2:64}]".format(item["no"], item["created"], item["url"]))
+    print("Total:{:>10}".format(len(tek_zip_list)))
 
-def print_tek_bin_full(tek_bin):
+def print_tek_bin_full(tek_bin) -> None:
     print("start_timestamp: [{:%Y-%m-%d %H:%M:%S%z}]".format(datetime.fromtimestamp(tek_bin.start_timestamp).astimezone()))
     print("end_timestamp:   [{:%Y-%m-%d %H:%M:%S%z}]".format(datetime.fromtimestamp(tek_bin.end_timestamp).astimezone()))
     print("region:          [{:}]".format(tek_bin.region))
@@ -88,38 +104,41 @@ def print_tek_bin_full(tek_bin):
 
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description='Probe TemporaryExposureKeys and Files of Exposure Notifications System in Japan a.k.a. "COCOA".', prefix_chars='-/')
     parser.add_argument('TEK', default=None, nargs='?', help="TEK Zip URL. If not set, print TEK distributed list")
+    parser.add_argument("-ekc", "--each-keys-count", action='store_true', dest="ekc", help="Print keys count each zip with TEK distributed list. Only available when TEK distributed list.")
     parser.add_argument("-v", "--version", action="version", version=__version__)
     args = parser.parse_args(sys.argv[1:])
 
-    try:
-        if args.TEK is None:
-            df = normalize_dataframe(get_tek_json(_tek_url))
-            df_dict = df.to_dict(orient="records")
+    if args.TEK is None:
+        try:
+            df = normalize_dataframe(get_url_json(_tek_distributed_url))
+            tek_zip_list = df.to_dict(orient="records")
+            print_tek_zip_list(tek_zip_list, args.ekc)
+        except Exception as e:
+            print("Error happens, when getting TEK distributed list.")
+            tb = sys.exc_info()[2]
+            print(e.with_traceback(tb))
+            return 1
 
-            print("{0:4}  {1:28} {2:}".format("#", "Created", "TEK URL"))
-            for item in df_dict:
-                print("{0:4}  [{1:%Y-%m-%d %H:%M:%S%z}]   [{2:}]".format(item["no"], item["created"], item["url"]))
-            print("Total:{:>10}".format(len(df_dict)))
-
-        elif _isURL(args.TEK):
-            key_bin, _ = extract_key_zip(sys.argv[1])
+    elif _isURL(args.TEK):
+        try:
+            key_bin, _ = extract_key_zip(args.TEK)
             tek_bin = TemporaryExposureKeyExport()
             tek_bin.ParseFromString(key_bin)
             print_tek_bin_full(tek_bin)
-        else:
-            print("Argument should be valid URL for TEK.")
-            sys.exit(1)
-
-    
-    except Exception as e:
-            print("Error happens, when getting TEK JSON.")
+        except Exception as e:
+            print("Error happens, when getting TEK ZIP detail.")
             tb = sys.exc_info()[2]
             print(e.with_traceback(tb))
-            sys.exit(1)
+            return 1
+    else:
+        print("Argument should be valid URL for TEK.")
+        return 1
+
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
