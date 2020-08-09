3e8d92c5da744244611c4eaff7d6ad9733160cfa
rocaz <rocaznet@gmail.com>
2020-08-10 00:09:22 +0900

    change specification of dl option

--- probeCOCOATek/probeCOCOATek.py
+++ probeCOCOATek/probeCOCOATek.py
@@ -18,7 +18,7 @@ from TemporaryExposureKey.TemporaryExposureKey_pb2 import TemporaryExposureKeyEx
 
 import probeCOCOATek
 
-__version__ = '3.20200809'
+__version__ = '4.20200809'
 
 _interval_sec_in_japan = 1
 
@@ -43,7 +43,7 @@ class NoneValueError(Error):
         self.message = message
 
 
-def _isURL(url) -> bool:
+def _isURL(url:str) -> bool:
     try:
         u = urlparse(url)
     except:
@@ -51,20 +51,19 @@ def _isURL(url) -> bool:
     return True
 
 
-def get_url_content(url) -> bytes:
+def get_url_content(url:str) -> bytes:
     time.sleep(_interval_sec_in_japan)
     return requests.get(url, stream=True).content
 
 
-def extract_key_zip(zip_url):
-    c = get_url_content(zip_url)
-    z = zipfile.ZipFile(io.BytesIO(c))
+def extract_key_zip(zip_content:bytes) -> (bytes, bytes):
+    z = zipfile.ZipFile(io.BytesIO(zip_content))
     return z.read(_tek_key).strip("{:16}".format("EK Export v1").encode('utf-8')), z.read(_tek_sig)
 
 
-def normalize_dataframe(js) -> pd.DataFrame:
+def normalize_dataframe(js_content:dict) -> pd.DataFrame:
     tek_list = []
-    for item in js:
+    for item in js_content:
         tek = {"created":datetime.fromtimestamp(round(item["created"] / 1000)).astimezone(), "url": item["url"]}
         tek_list.append(tek)
     df = pd.DataFrame(tek_list)
@@ -76,12 +75,13 @@ def normalize_dataframe(js) -> pd.DataFrame:
     return df
 
 
-def download_zips(tek_distribution_url) -> None:
-    dtname = "{:%Y%m%d_%H%M%S}".format(datetime.now())
-    dir_name = os.path.join(os.getcwd(), dtname)
-    js = get_url_content(tek_distribution_url).decode('utf-8')
+def download_zips(tek_distribution_url:str, dir_base:str) -> None:
+    dir_name = os.path.abspath(dir_base)
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 
-    os.makedirs(dir_name, exist_ok=True)
+    dtname = "{:%Y%m%d_%H%M%S}".format(datetime.now())
+    js = get_url_content(tek_distribution_url).decode('utf-8')
 
     with open(os.path.join(dir_name, dtname+".json"), 'w') as f:
         f.write(js)
@@ -94,12 +94,13 @@ def download_zips(tek_distribution_url) -> None:
         f.write(os.linesep.join(text_lines))
     
     for item in tek_zip_list:
-        z = get_url_content(item["url"])
-        with open(os.path.join(dir_name, os.path.basename(item["url"])), 'wb') as f:
-            f.write(z)
+        if not os.path.exists(os.path.join(dir_name, os.path.basename(item["url"]))):
+            z = get_url_content(item["url"])
+            with open(os.path.join(dir_name, os.path.basename(item["url"])), 'wb') as f:
+                f.write(z)
 
 
-def print_tek_zip_list(tek_zip_list, ekc) -> list:
+def print_tek_zip_list(tek_zip_list:dict, ekc:bool) -> list:
     text_lines = []
 
     if ekc:
@@ -108,7 +109,8 @@ def print_tek_zip_list(tek_zip_list, ekc) -> list:
         text_lines.append("{0:4}  {1:28} {2:64}".format("#", "Created", "ZIP URL"))
     for item in tek_zip_list:
         if ekc:
-            key_bin, _ = extract_key_zip(item["url"])
+            zip_content = get_url_content(item["url"])
+            key_bin, _ = extract_key_zip(zip_content)
             tek_bin = TemporaryExposureKeyExport()
             tek_bin.ParseFromString(key_bin)
             cnt = len(tek_bin.keys)
@@ -120,13 +122,14 @@ def print_tek_zip_list(tek_zip_list, ekc) -> list:
     return text_lines
 
 
-def print_tek_keys_list(tek_zip_list) -> list:
+def print_tek_keys_list(tek_zip_list:dict) -> list:
     text_lines = []
 
     text_lines.append("{0:11}  {1:28} {2:32}".format("#", "Created", "TEK Data"))
     key_cnt = 0
     for item in tek_zip_list:
-        key_bin, _ = extract_key_zip(item["url"])
+        zip_content = get_url_content(item["url"])
+        key_bin, _ = extract_key_zip(zip_content)
         tek_bin = TemporaryExposureKeyExport()
         tek_bin.ParseFromString(key_bin)
         for k in tek_bin.keys:
@@ -138,7 +141,7 @@ def print_tek_keys_list(tek_zip_list) -> list:
     return text_lines
 
 
-def print_tek_bin_detail(tek_bin) -> str:
+def print_tek_bin_detail(tek_bin:TemporaryExposureKeyExport) -> str:
     text_lines = []
 
     text_lines.append("start_timestamp: [{:%Y-%m-%d %H:%M:%S%z}]".format(datetime.fromtimestamp(tek_bin.start_timestamp).astimezone()))
@@ -165,13 +168,13 @@ def main() -> None:
     parser.add_argument("-z", "--zip-url", default=None, dest="zip_url", help="print TEK ZIP Detail. If not set, print TEK distribution list")
     parser.add_argument("-ekc", "--each-keys-count", action='store_true', dest="ekc", help="Print keys count each ZIP with TEK distribution list. Only available when TEK distribution list.")
     parser.add_argument("-akl", "--all-keys-list", action='store_true', dest="akl", help="Print a list of all keys for each ZIP. Other options are ignored.")
-    parser.add_argument("-dl", "--dl-zip", action='store_true', dest="dl", help="Download all TEK ZIP and list JSON from TEK distribution list into folder named with datetime. Other options are ignored.")
+    parser.add_argument("-dl", "--dl-zip", default=None, dest="dl_dir", help="Specified directory for downloading all TEK ZIP and list JSON from TEK distribution list. Other options are ignored.")
     parser.add_argument("-v", "--version", action="version", version=__version__)
     args = parser.parse_args(sys.argv[1:])
 
-    if args.dl:
+    if args.dl_dir is not None:
         try:
-            download_zips(_tek_distribution_url)
+            download_zips(_tek_distribution_url, args.dl_dir)
             print("Download done.")
         except Exception as e:
             print("Error happens, when downloading TEK ZIP and distribution list.")
@@ -196,7 +199,8 @@ def main() -> None:
 
     elif _isURL(args.zip_url):
         try:
-            key_bin, _ = extract_key_zip(args.zip_url)
+            zip_content = get_url_content(args.zip_url)
+            key_bin, _ = extract_key_zip(zip_content)
             tek_bin = TemporaryExposureKeyExport()
             tek_bin.ParseFromString(key_bin)
             text_lines = print_tek_bin_detail(tek_bin)
